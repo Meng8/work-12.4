47b7f1afd1c20f4493b378247d702d217255d8d3
Meng8 <1098167361@qq.com>
2017-12-05 22:21:41 +0800

    完善

--- src/main/java/com/example/controller/JsonAllTest.java
+++ src/main/java/com/example/controller/JsonAllTest.java
@@ -1,13 +1,15 @@
 package com.example.controller;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.example.entity.User;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -23,10 +25,24 @@ public class JsonAllTest {
 		return "table";
 	}
 	
-	
+	//接收前台 json数据转为entity
+		@RequestMapping("/getall")
+		@ResponseBody
+		public String get(String json) {
+			User user=new User();
+			try {
+				user = om.readValue(json, User.class);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			System.out.println(user);
+			return "success";
+		}
+		
+	//	吧entity转换成json到前台
 	@RequestMapping(value="/testall")
 	@ResponseBody
-	public List<User> produceUse() {
+	public List<User> produceUse(HttpServletResponse resp) throws IOException {
 		System.out.println("----我是一道美丽的分割线----");
 		List<User> users= new ArrayList<User>();
 		for(int i=1;i<=10;i++) {
@@ -41,6 +57,7 @@ public class JsonAllTest {
 		}
 		users.add(user);
 		}
+		resp.getWriter().println(users); 
 		return users;
 	}
 
